1ddb8e4d62f1ed55f326d210b318ad5ad5038c7b
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-11 16:12:25 -0400

    Actualización de Repository para usar JpaRepository

--- src/main/java/com/ranking/microservicio_ranking/repository/RankingRepository.java
+++ src/main/java/com/ranking/microservicio_ranking/repository/RankingRepository.java
@@ -1,31 +1,11 @@
 package com.ranking.microservicio_ranking.repository;
 
 import com.ranking.microservicio_ranking.model.Ranking;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository
-public class RankingRepository {
-
-    private List<Ranking> listaRanking = new ArrayList<>();
-
-    public List<Ranking> obtenerTodo(){
-        return listaRanking;
-    }
-
-    public Ranking buscarPorId(Long id){
-        for(Ranking ranking: listaRanking){
-            if(ranking.getIdUsuario().equals(id)){
-                return ranking;
-            }
-        }
-        return null;
-    }
-
-    public void guardarRanking(Ranking ranking){
-        listaRanking.add(ranking);
-    }
+public interface RankingRepository extends JpaRepository<Ranking, Long> {
 
 }
